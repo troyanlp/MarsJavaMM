e0dc9957389cc0b23667b2da6ff74be77efc6c52
Manuel Méndez <troyanlp@gmail.com>
2017-12-13 19:13:39 +0100

    Rover only moves when the next position is inside the grid.

--- src/DataProcessor.java
+++ src/DataProcessor.java
@@ -14,6 +14,7 @@ public class DataProcessor {
 		this.data = input;
 		this.gridX = x;
 		this.gridY = y;
+		manager.SetGrid(x, y);
 	}
 	
 	public void ProcessData() {
--- src/RoverManager.java
+++ src/RoverManager.java
@@ -2,7 +2,9 @@ import java.util.LinkedList;
 
 public class RoverManager {
 	
-	private LinkedList<Rover> rovers =  new LinkedList<>();;
+	private LinkedList<Rover> rovers =  new LinkedList<>();
+	private int width = 0;
+	private int height = 0;
 	
 	public RoverManager() {
 		
@@ -15,15 +17,41 @@ public class RoverManager {
 	
 	public void MoveRover(char order) {
 		if(order == 'M' || order == 'm') {
-			rovers.getLast().Move();
+			Rover aux = rovers.getLast();
+			if(CanMove()) aux.Move();
 		}else {
 			rovers.getLast().Turn(order == 'R' || order == 'r');
 		}
 	}
 	
+	public boolean CanMove() {
+		//Check if is inside the grid
+		Rover aux = rovers.getLast();
+		switch(aux.getHeading()) {
+		case 'N':
+			if((aux.getY() + 1) <= height) return true;
+			break;
+		case 'S':
+			if((aux.getY() - 1) > 0) return true;
+			break;
+		case 'E':
+			if((aux.getX() + 1) <= width) return true;
+			break;
+		case 'W':
+			if((aux.getX() - 1) > 0) return true;
+			break;
+		}
+		return false;
+	}
+	
 	public void PrintOutput() {
 		Rover aux = rovers.getLast();
 		System.out.println(aux.getX() + " " + aux.getY() + " " + aux.getHeading());
+	}
+	
+	public void SetGrid(int x, int y) {
+		this.width = x;
+		this.height = y;
 	}
 
 }
