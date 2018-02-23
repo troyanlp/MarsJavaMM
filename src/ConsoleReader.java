575f85add16c43279832546f2670662515338ac4
Manuel Méndez <troyanlp@gmail.com>
2018-02-23 01:20:11 +0100

    Solved bug that made possible negative grid size values and crashed when a not number was entered in console mode.

--- src/ConsoleReader.java
+++ src/ConsoleReader.java
@@ -12,26 +12,31 @@ public class ConsoleReader implements IReader {
 		// TODO Auto-generated method stub
 		System.out.println("Console Reader");
 		
-		
-		
 		System.out.println("Enter the size of the grid: ");
 		Scanner reader = new Scanner(System.in);
-		int width = reader.nextInt();
-		int height = reader.nextInt();
-		System.out.println(width + " " + height);
-		LinkedList<String> inputs = new LinkedList<>();
-		System.out.println("Write Mars Rovers data and when you're done write 'stop'");
-		while(reader.hasNextLine()) {
-			inputs.add(reader.nextLine());
-			if(inputs.getLast().compareToIgnoreCase("stop") == 0) {
-				inputs.poll();
-				inputs.pollLast();
-				break;
+		int width = -1, height = -1;
+		if(reader.hasNextInt()) width = reader.nextInt();
+		if(reader.hasNextInt()) height = reader.nextInt();
+		
+		if(width > 0 && height > 0) {
+			//System.out.println(width + " " + height);
+			LinkedList<String> inputs = new LinkedList<>();
+			System.out.println("Write Mars Rovers data and when you're done write 'stop'");
+			while(reader.hasNextLine()) {
+				inputs.add(reader.nextLine());
+				if(inputs.getLast().compareToIgnoreCase("stop") == 0) {
+					inputs.poll();
+					inputs.pollLast();
+					break;
+				}
 			}
+			DataProcessor processor = new DataProcessor();
+			processor.LoadData(inputs, width, height);
+			processor.ProcessData();
+		}else {
+			System.out.println("No valid grid size values.");
 		}
-		DataProcessor processor = new DataProcessor();
-		processor.LoadData(inputs, width, height);
-		processor.ProcessData();
+		
 		reader.close(); 
 	}
 
